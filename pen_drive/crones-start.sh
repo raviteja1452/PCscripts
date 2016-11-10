6459a79a1538ae1daa073b78bb2187d28077490d
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-10 19:13:55 +0530

    changes in shell scripts

--- pen_drive/crones-start.sh
+++ pen_drive/crones-start.sh
@@ -1,9 +1,9 @@
-tmux new-session -d -s amazon-pendrive 'sh amazon.sh'
-tmux new-session -d -s snapdeal-pendrive 'sh snapdeal.sh'
-tmux new-session -d -s flipkart-pendrive 'sh flipkart.sh'
-tmux new-session -d -s croma-pendrive 'sh croma.sh'
-tmux new-session -d -s paytm-pendrive 'sh paytm.sh'
-tmux new-session -d -s ebay-pendrive 'sh ebay.sh'
-tmux new-session -d -s infibeam-pendrive 'sh infibeam.sh'
+tmux new-session -d -s amazon-pendrive './amazon.sh'
+tmux new-session -d -s snapdeal-pendrive './snapdeal.sh'
+tmux new-session -d -s flipkart-pendrive './flipkart.sh'
+tmux new-session -d -s croma-pendrive './croma.sh'
+tmux new-session -d -s paytm-pendrive './paytm.sh'
+tmux new-session -d -s ebay-pendrive './ebay.sh'
+tmux new-session -d -s infibeam-pendrive './infibeam.sh'
 
 
--- tv/crones.sh
+++
@@ -1,9 +0,0 @@
-tmux new-session -d -s amazon-tv 'sh amazon.sh'
-tmux new-session -d -s snapdeal-tv 'sh snapdeal.sh'
-tmux new-session -d -s flipkart-tv 'sh flipkart.sh'
-tmux new-session -d -s croma-tv 'sh croma.sh'
-tmux new-session -d -s paytm-tv 'sh paytm.sh'
-tmux new-session -d -s ebay-tv 'sh ebay.sh'
-tmux new-session -d -s infibeam-tv 'sh infibeam.sh'
-
-
