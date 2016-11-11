5629340c499d1d880d24fdfbc8fce93963c5407b
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-11 13:36:52 +0530

    added monitor and headphone categories

---
+++ headphone/crones-stop.sh
@@ -0,0 +1,7 @@
+tmux kill-session -t amazon-headphone
+tmux kill-session -t snapdeal-headphone 
+tmux kill-session -t flipkart-headphone 
+tmux kill-session -t croma-headphone 
+tmux kill-session -t paytm-headphone 
+tmux kill-session -t ebay-headphone
+tmux kill-session -t infibeam-headphone 
---
+++ monitor/crones-stop.sh
@@ -0,0 +1,7 @@
+tmux kill-session -t amazon-monitor
+tmux kill-session -t snapdeal-monitor 
+tmux kill-session -t flipkart-monitor 
+tmux kill-session -t croma-monitor 
+tmux kill-session -t paytm-monitor 
+tmux kill-session -t ebay-monitor
+tmux kill-session -t infibeam-monitor 
