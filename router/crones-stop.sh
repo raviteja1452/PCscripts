71d99d0cb59818dc12e4f75d7caf481afe902eb9
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-11 17:04:06 +0530

    added router and print scripts

---
+++ router/crones-stop.sh
@@ -0,0 +1,7 @@
+tmux kill-session -t amazon-router
+tmux kill-session -t snapdeal-router 
+tmux kill-session -t flipkart-router 
+tmux kill-session -t croma-router 
+tmux kill-session -t paytm-router 
+tmux kill-session -t ebay-router
+tmux kill-session -t infibeam-router 
