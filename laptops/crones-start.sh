2963c3eb5b707cb97c23790a3658d17dc394c36d
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-14 12:48:55 +0530

    tv crones-start change

--- laptops/crones-start.sh
+++ laptops/crones-start.sh
@@ -1,9 +1,9 @@
-tmux new-session -d -s amazon-laptops 'sh amazon.sh'
-tmux new-session -d -s snapdeal-laptops 'sh snapdeal.sh'
-tmux new-session -d -s flipkart-laptops 'sh flipkart.sh'
-tmux new-session -d -s croma-laptops 'sh croma.sh'
-tmux new-session -d -s paytm-laptops 'sh paytm.sh'
-tmux new-session -d -s ebay-laptops 'sh ebay.sh'
-tmux new-session -d -s infibeam-laptops 'sh infibeam.sh'
+tmux new-session -d -s amazon-laptops './amazon.sh'
+tmux new-session -d -s snapdeal-laptops './snapdeal.sh'
+tmux new-session -d -s flipkart-laptops './flipkart.sh'
+tmux new-session -d -s croma-laptops './croma.sh'
+tmux new-session -d -s paytm-laptops './paytm.sh'
+tmux new-session -d -s ebay-laptops './ebay.sh'
+tmux new-session -d -s infibeam-laptops './infibeam.sh'
 
 
