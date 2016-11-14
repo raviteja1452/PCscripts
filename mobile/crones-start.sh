a93c550b956b9999ab56b38db845ff9cac204dfa
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-14 12:47:34 +0530

    changes in crones-start

--- mobile/crones-start.sh
+++ mobile/crones-start.sh
@@ -1,9 +1,9 @@
-tmux new-session -d -s amazon-mobile 'sh amazon.sh'
-tmux new-session -d -s snapdeal-mobile 'sh snapdeal.sh'
-tmux new-session -d -s flipkart-mobile 'sh flipkart.sh'
-tmux new-session -d -s croma-mobile 'sh croma.sh'
-tmux new-session -d -s paytm-mobile 'sh paytm.sh'
-tmux new-session -d -s ebay-mobile 'sh ebay.sh'
-tmux new-session -d -s infibeam-mobile 'sh infibeam.sh'
+tmux new-session -d -s amazon-mobile './amazon.sh'
+tmux new-session -d -s snapdeal-mobile './snapdeal.sh'
+tmux new-session -d -s flipkart-mobile './flipkart.sh'
+tmux new-session -d -s croma-mobile './croma.sh'
+tmux new-session -d -s paytm-mobile './paytm.sh'
+tmux new-session -d -s ebay-mobile './ebay.sh'
+tmux new-session -d -s infibeam-mobile './infibeam.sh'
 
 
