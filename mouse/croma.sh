7bd07e2be374bfcbbd41f2a1fd12f630fb89d5f4
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-15 14:33:55 +0530

    mouse config

--- mouse/croma.sh
+++ mouse/croma.sh
@@ -1,9 +1,6 @@
 cd /home/ubuntu/PCScraper/scrapperservice
 source /home/ubuntu/PCScraper/scrapperservice/scrapperservice/bin/activate
 
-<<<<<<< HEAD
 PYTHONPATH=/home/ubuntu/PCScraper/scrapperservice python /home/ubuntu/PCScraper/scrapperservice/scheduler/Main.py -c 14 -w 4
-../end_script.sh
-=======
-PYTHONPATH=/home/ubuntu/PCScraper/scrapperservice python /home/ubuntu/PCScraper/scrapperservice/scheduler/Main.py -c 13 -w 4
->>>>>>> 308c1613a512d34caa90738b15159fd768f52850
+.cd ~/PCscripts
+./end_script.sh
