b2640dc948b0941813fbaf47c32cfb377ab92bff
harinath <coupondunia@CouponDunias-MacBook-Pro-2.local>
2016-11-15 13:20:22 +0530

    changes done to auqarium script

--- installation/install-aquarium.sh
+++ installation/install-aquarium.sh
@@ -1,6 +1,8 @@
 sudo cd ~
 sudo  curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > ./docker-compose
 sudo cp ./docker-compose /usr/local/bin/docker-compose
+cd ~
+rm -r ./docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
