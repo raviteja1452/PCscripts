41e4ed4aad51b5163afe37b63502102944444fd7
harinath <coupondunia@CouponDunias-MacBook-Pro-2.local>
2016-11-15 13:46:21 +0530

    repo installation script added

---
+++ installation/install-repos.sh
@@ -0,0 +1,6 @@
+cd ~
+git clone https://github.com/CouponDunia/PCScraper.git
+cd PCScraper/scrapperservice
+virtualenv scrapperservice
+source scrapperservice/bin/activate
+pip install -r requirements.txt
