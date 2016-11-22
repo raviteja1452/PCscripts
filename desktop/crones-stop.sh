6f3828151bbd9d79f3d848171d7adf6c55881dff
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-22 13:09:50 +0530

    desktop scripts

---
+++ desktop/crones-stop.sh
@@ -0,0 +1,7 @@
+tmux kill-session -t amazon-desktop
+tmux kill-session -t snapdeal-desktop 
+tmux kill-session -t flipkart-desktop 
+tmux kill-session -t croma-desktop 
+tmux kill-session -t paytm-desktop 
+tmux kill-session -t ebay-desktop
+tmux kill-session -t infibeam-desktop 
