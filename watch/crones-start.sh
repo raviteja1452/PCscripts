e65175dc3096b12fad2f757e48fee53b6b929ad4
raviteja1452 <b.raviteja1452@gmail.com>
2016-11-28 15:47:38 +0530

    start and stop cron scripts for watch , projector and game_console

---
+++ watch/crones-start.sh
@@ -0,0 +1,8 @@
+tmux new-session -d -s amazon-scanner './amazon.sh'
+tmux new-session -d -s snapdeal-scanner './snapdeal.sh'
+tmux new-session -d -s flipkart-scanner './flipkart.sh'
+tmux new-session -d -s paytm-scanner './paytm.sh'
+tmux new-session -d -s ebay-scanner './ebay.sh'
+tmux new-session -d -s infibeam-scanner './infibeam.sh'
+
+
---
+++ watch/crones-stop.sh
@@ -0,0 +1,6 @@
+tmux kill-session -t amazon-scanner
+tmux kill-session -t snapdeal-scanner 
+tmux kill-session -t flipkart-scanner 
+tmux kill-session -t paytm-scanner 
+tmux kill-session -t ebay-scanner
+tmux kill-session -t infibeam-scanner 
