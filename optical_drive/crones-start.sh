a15090f828053d86364a17719fc87c3cd38ba01b
raviteja1452 <b.raviteja1452@gmail.com>
2016-12-05 13:27:14 +0530

    optical_drive

---
+++ optical_drive/crones-start.sh
@@ -0,0 +1,6 @@
+tmux new-session -d -s amazon-odrive './amazon.sh'
+tmux new-session -d -s snapdeal-odrive './snapdeal.sh'
+tmux new-session -d -s flipkart-odrive './flipkart.sh'
+tmux new-session -d -s ebay-odrive './ebay.sh'
+
+
