79f21345fdd43ddcaf5217507a6582d91fb7e52d
raviteja1452 <b.raviteja1452@gmail.com>
2016-12-06 16:31:06 +0530

    gaming accesories

---
+++ gaming/crones-start.sh
@@ -0,0 +1,9 @@
+tmux new-session -d -s amazon-gaming './amazon.sh'
+tmux new-session -d -s snapdeal-gaming './snapdeal.sh'
+tmux new-session -d -s flipkart-gaming './flipkart.sh'
+tmux new-session -d -s croma-gaming './croma.sh'
+tmux new-session -d -s paytm-gaming './paytm.sh'
+tmux new-session -d -s ebay-gaming './ebay.sh'
+tmux new-session -d -s infibeam-gaming './infibeam.sh'
+
+
