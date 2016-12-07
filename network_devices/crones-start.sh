4e0bb21e908e13b1c14ff06789919d3c8592040d
raviteja1452 <b.raviteja1452@gmail.com>
2016-12-07 12:59:42 +0530

    networking devices shell script

---
+++ network_devices/crones-start.sh
@@ -0,0 +1,9 @@
+tmux new-session -d -s amazon-network './amazon.sh'
+tmux new-session -d -s snapdeal-network './snapdeal.sh'
+tmux new-session -d -s flipkart-network './flipkart.sh'
+tmux new-session -d -s croma-network './croma.sh'
+tmux new-session -d -s paytm-network './paytm.sh'
+tmux new-session -d -s ebay-network './ebay.sh'
+tmux new-session -d -s infibeam-network './infibeam.sh'
+
+
