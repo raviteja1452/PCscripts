748eafd034e134f81002ae122df30fa80b13caa8
raviteja1452 <b.raviteja1452@gmail.com>
2016-12-07 13:07:42 +0530

    optical drive changes

--- optical_drive/crones-stop.sh
+++ optical_drive/crones-stop.sh
@@ -1,4 +1,4 @@
 tmux kill-session -t amazon-odrive
 tmux kill-session -t snapdeal-odrive 
-tmux kill-session -t paytm-odrive 
+tmux kill-session -t flipkart-odrive 
 tmux kill-session -t ebay-odrive
