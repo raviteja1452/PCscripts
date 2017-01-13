11e6a531879ec266cb14737218e5be183736ed91
raviteja1452 <b.raviteja1452@gmail.com>
2017-01-13 13:04:56 +0530

    pcscripts

--- desktop/snapdeal.sh
+++ desktop/snapdeal.sh
@@ -1,6 +1,6 @@
-cd /home/ubuntu/PCScraper/scrapperservice
-source /home/ubuntu/PCScraper/scrapperservice/scrapperservice/bin/activate
+cd /home/pc/PCScraper
+source /home/pc/PCScraper/scraperservice/bin/activate
 
-PYTHONPATH=/home/ubuntu/PCScraper/scrapperservice python /home/ubuntu/PCScraper/scrapperservice/scheduler/Main.py -c 17 -w 2
+PYTHONPATH=/home/pc/PCScraper pypy /home/pc/PCScraper/scheduler/Main.py -c 17 -w 2
 cd ~/PCscripts
 ./end_script.sh
--- watch/crones-start.sh
+++ watch/crones-start.sh
@@ -1,7 +1,7 @@
 tmux new-session -d -s amazon-watch './amazon.sh'
 tmux new-session -d -s snapdeal-watch './snapdeal.sh'
 tmux new-session -d -s flipkart-watch './flipkart.sh'
-tmux new-session -d -s croma-watch './paytm.sh'
+tmux new-session -d -s croma-watch './croma.sh'
 tmux new-session -d -s paytm-watch './paytm.sh'
 tmux new-session -d -s ebay-watch './ebay.sh'
 tmux new-session -d -s infibeam-watch './infibeam.sh'
