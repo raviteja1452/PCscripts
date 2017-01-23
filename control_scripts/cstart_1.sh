78a6269819118355157ab26ce084885ca1a45d0c
Raviteja Bejgum <ravi1452@ravi1452.local>
2017-01-23 15:52:52 +0530

    control scripts added

---
+++ control_scripts/cstart_1.sh
@@ -0,0 +1,20 @@
+~/PCscripts/mobile/crones-start.sh
+sleep 2
+~/PCscripts/laptops/crones-start.sh
+sleep 2
+~/PCscripts/tv/crones-start.sh
+sleep 2
+~/PCscripts/external_hd/crones-start.sh
+sleep 2
+~/PCscripts/mem_card/crones-start.sh
+sleep 2
+~/PCscripts/pen_drive/crones-start.sh
+sleep 2
+~/PCscripts/keyboard/crones-start.sh
+sleep 2
+~/PCscripts/headphone/crones-start.sh
+sleep 2
+~/PCscripts/monitor/crones-start.sh
+sleep 2
+~/PCscripts/printer/crones-start.sh
+sleep 2
---
+++ control_scripts/cstop.sh
@@ -0,0 +1,20 @@
+~/PCscripts/mobile/crones-stop.sh
+sleep 2
+~/PCscripts/laptops/crones-stop.sh
+sleep 2
+~/PCscripts/tv/crones-stop.sh
+sleep 2
+~/PCscripts/external_hd/crones-stop.sh
+sleep 2
+~/PCscripts/mem_card/crones-stop.sh
+sleep 2
+~/PCscripts/pen_drive/crones-stop.sh
+sleep 2
+~/PCscripts/keyboard/crones-stop.sh
+sleep 2
+~/PCscripts/headphone/crones-stop.sh
+sleep 2
+~/PCscripts/monitor/crones-stop.sh
+sleep 2
+~/PCscripts/printer/crones-stop.sh
+sleep 2
