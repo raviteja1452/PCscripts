698b5114bffd347a3e3af10d20db4fc9bf5c0702
Raviteja Bejgum <ravi1452@ravi1452.local>
2017-01-23 17:17:31 +0530

    game console

--- game_console/crones-start.sh
+++ game_console/crones-start.sh
@@ -1,9 +1,10 @@
-tmux new-session -d -s amazon-game './amazon.sh'
-tmux new-session -d -s snapdeal-game './snapdeal.sh'
-tmux new-session -d -s flipkart-game './flipkart.sh'
-tmux new-session -d -s croma-game './croma.sh'
-tmux new-session -d -s paytm-game './paytm.sh'
-tmux new-session -d -s ebay-game './ebay.sh'
-tmux new-session -d -s infibeam-game './infibeam.sh'
-
+tmux new-session -d -s amazon-gconsole './amazon.sh'
+tmux new-session -d -s snapdeal-gconsole './snapdeal.sh'
+tmux new-session -d -s flipkart-gconsole './flipkart.sh'
+tmux new-session -d -s croma-gconsole './croma.sh'
+tmux new-session -d -s paytm-gconsole './paytm.sh'
+tmux new-session -d -s ebay-gconsole './ebay.sh'
+tmux new-session -d -s infibeam-gconsole './infibeam.sh'
+tmux new-session -d -s shopclues-gconsole './shopclues.sh'
+ 
 
