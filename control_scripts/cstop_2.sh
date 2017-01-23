44b3d46ed3b30530a1fd7a5f62a7e03ea2ef5a81
Raviteja Bejgum <ravi1452@ravi1452.local>
2017-01-23 17:38:16 +0530

    control statements

---
+++ control_scripts/cstop_2.sh
@@ -0,0 +1,59 @@
+echo 'router'
+cd ~/PCscripts/router
+~/PCscripts/router/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'mouse'
+cd ~/PCscripts/mouse
+~/PCscripts/mouse/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'home theater'
+cd ~/PCscripts/home_theater
+~/PCscripts/home_theater/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'speaker'
+cd ~/PCscripts/speaker
+~/PCscripts/speaker/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'tablet'
+cd ~/PCscripts/tablets
+~/PCscripts/tablets/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'desktop'
+cd ~/PCscripts/desktop
+~/PCscripts/desktop/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'scanner'
+cd ~/PCscripts/scanner
+~/PCscripts/scanner/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'watch'
+cd ~/PCscripts/watch
+~/PCscripts/watch/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'game console'
+cd ~/PCscripts/game_console
+~/PCscripts/game_console/crones-stop.sh
+tmux ls
+sleep 2
+
+echo 'projector'
+cd ~/PCscripts/projector
+~/PCscripts/projector/crones-stop.sh
+tmux ls
+sleep 2
