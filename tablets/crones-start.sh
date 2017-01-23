9e0d5b5b6171bfff76a3175161dc0dd00c54bf4d
Raviteja Bejgum <ravi1452@ravi1452.local>
2017-01-23 16:49:59 +0530

    tablets

--- tablets/crones-start.sh
+++ tablets/crones-start.sh
@@ -1,9 +1,10 @@
-tmux new-session -d -s amazon-tablets './amazon.sh'
-tmux new-session -d -s snapdeal-tablets './snapdeal.sh'
-tmux new-session -d -s flipkart-tablets './flipkart.sh'
-tmux new-session -d -s croma-tablets './croma.sh'
-tmux new-session -d -s paytm-tablets './paytm.sh'
-tmux new-session -d -s ebay-tablets './ebay.sh'
-tmux new-session -d -s infibeam-tablets './infibeam.sh'
-
+tmux new-session -d -s amazon-tablet './amazon.sh'
+tmux new-session -d -s snapdeal-tablet './snapdeal.sh'
+tmux new-session -d -s flipkart-tablet './flipkart.sh'
+tmux new-session -d -s croma-tablet './croma.sh'
+tmux new-session -d -s paytm-tablet './paytm.sh'
+tmux new-session -d -s ebay-tablet './ebay.sh'
+tmux new-session -d -s infibeam-tablet './infibeam.sh'
+tmux new-session -d -s shopclues-tablet './shopclues.sh'
+ 
 
