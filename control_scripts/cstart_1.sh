8dc97a2779cbf6db29f62e59f032cbd204cf96f3
Raviteja Bejgum <ravi1452@ravi1452.local>
2017-01-23 16:01:58 +0530

    changes in control scripts

--- control_scripts/cstart_1.sh
+++ control_scripts/cstart_1.sh
@@ -1,20 +1,30 @@
+echo 'mobiles'
 ~/PCscripts/mobile/crones-start.sh
 sleep 2
+echo 'laptops'
 ~/PCscripts/laptops/crones-start.sh
 sleep 2
+echo 'tv'
 ~/PCscripts/tv/crones-start.sh
 sleep 2
+echo 'external card'
 ~/PCscripts/external_hd/crones-start.sh
 sleep 2
+echo 'memory card'
 ~/PCscripts/mem_card/crones-start.sh
 sleep 2
+echo 'pendrive'
 ~/PCscripts/pen_drive/crones-start.sh
 sleep 2
+echo 'keyboard'
 ~/PCscripts/keyboard/crones-start.sh
 sleep 2
+echo 'headphone'
 ~/PCscripts/headphone/crones-start.sh
 sleep 2
+echo 'monitor'
 ~/PCscripts/monitor/crones-start.sh
 sleep 2
+echo 'printer'
 ~/PCscripts/printer/crones-start.sh
 sleep 2
--- control_scripts/cstop.sh
+++ control_scripts/cstop.sh
@@ -1,20 +1,30 @@
+echo 'mobiles'
 ~/PCscripts/mobile/crones-stop.sh
 sleep 2
+echo 'laptops'
 ~/PCscripts/laptops/crones-stop.sh
 sleep 2
+echo 'tv'
 ~/PCscripts/tv/crones-stop.sh
 sleep 2
+echo 'external card'
 ~/PCscripts/external_hd/crones-stop.sh
 sleep 2
+echo 'memory card'
 ~/PCscripts/mem_card/crones-stop.sh
 sleep 2
+echo 'pendrive'
 ~/PCscripts/pen_drive/crones-stop.sh
 sleep 2
+echo 'keyboard'
 ~/PCscripts/keyboard/crones-stop.sh
 sleep 2
+echo 'headphone'
 ~/PCscripts/headphone/crones-stop.sh
 sleep 2
+echo 'monitor'
 ~/PCscripts/monitor/crones-stop.sh
 sleep 2
+echo 'printer'
 ~/PCscripts/printer/crones-stop.sh
 sleep 2
